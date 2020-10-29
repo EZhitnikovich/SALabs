bdd3bc550a338de7cfeb3995d9e4e3228c335b08
Eugenii <72632604+EZhitnikovich@users.noreply.github.com>
2020-10-29 21:32:22 +0300

    add Lab7

---
+++ Lab7/Main.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int Find(string str, string templ)
+{
+    int  i, j, strlen, templlen;
+
+    strlen = str.length();
+    templlen = templ.length();
+    int* d = new int[templlen];
+
+    i = 0;
+    j = -1;
+    d[0] = -1;
+    while (i < templlen - 1)
+    {
+        while ((j >= 0) && (templ[j] != templ[i]))
+            j = d[j];
+        ++i;
+        ++j;
+        d[i] = (templ[i] == templ[j]) ? d[j] : j;
+    }
+
+    for (i = 0, j = 0; (i <= strlen - 1) && (j <= templlen - 1); ++i, ++j)
+        while ((j >= 0) && (templ[j] != str[i]))
+            j = d[j];
+    delete[] d;
+
+    return (j == templlen) ? i - j : -1;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    string source, pattern;
+
+    cout << "Введите ФИО: ";
+    getline(cin, source);
+
+    cout << "Введите шаблон: ";
+    getline(cin, pattern);
+
+    cout << Find(source, pattern);
+}
